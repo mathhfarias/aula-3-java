2432a4ebea27e4ffdaaeaf2313fcaec1540bcf6b
Matheus <matheusfariasdelima@gmail.com>
2023-09-26 09:01:25 -0300

    Switch, incremento

---
+++ script.js
@@ -0,0 +1,31 @@
+
+//switch
+
+//opção 4
+/*let opcao = prompt("Digite uma opção") 4;
+
+switch(opcao){
+    case 1:
+        document.write("Primeira opção")
+        break;
+    case 2:
+        document.write("Segunda opção")
+        break;
+    case 3:
+        document.write("Terceira opção")  
+        break;
+    default
+    document.write("Nenhuma das opções")   
+    break;           
+}
+*/
+
+let valor = 7
+console.log(valor)
+valor = valor-1
+console.log(valor);
+
+let valor1 =10
+console.log(valor1)
+valor +=3;
+console.log(valor1)
